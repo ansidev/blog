4cd9d4dc5287cd4476945c6a10ad24e53d363c61
Le Minh Tri <ansidev@gmail.com>
2022-02-04 13:51:21 +0700

    remove the plugin Bootstrap Icon

--- src/plugins/bootstrapIcon/index.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapIcons } from './icons'
-import { getBootstrapIconComponentOptions } from './utils'
-import type { UserModule } from '~/types'
-
-export const install: UserModule = ({ app }) => {
-  bootstrapIcons.forEach(({ name, path }) => {
-    app.component(`${name}-icon`, getBootstrapIconComponentOptions({ name, path }))
-  })
-}
