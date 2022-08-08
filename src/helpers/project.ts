1e9567df48faa5e445579bfec768e90ca54b373d
Le Minh Tri <ansidev@gmail.com>
2022-08-09 00:59:28 +0700

    feat(project): adjust development status badge colors

--- src/helpers/project.ts
+++ src/helpers/project.ts
@@ -3,22 +3,22 @@ export const getProjectBadgeStyle = (developmentStatus: string) => {
     case "active":
       return {
         backgroundColor: "green-100",
-        darkBackgroundColor: "green-900",
-        textColor: "green-900",
+        darkBackgroundColor: "green-800",
+        textColor: "green-800",
         darkTextColor: "green-100",
       }
     case "inactive":
       return {
         backgroundColor: "yellow-100",
-        darkBackgroundColor: "yellow-900",
-        textColor: "yellow-900",
+        darkBackgroundColor: "yellow-800",
+        textColor: "yellow-800",
         darkTextColor: "yellow-100",
       }
     case "unmaintained":
       return {
         backgroundColor: "red-100",
-        darkBackgroundColor: "red-900",
-        textColor: "red-900",
+        darkBackgroundColor: "red-800",
+        textColor: "red-800",
         darkTextColor: "red-100",
       }
     default:
