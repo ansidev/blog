137aef4722206947594e52838a3893c5a7751522
Le Minh Tri <ansidev@gmail.com>
2022-08-14 15:23:20 +0700

    feat: cache Google fonts

--- vite.config.ts
+++ vite.config.ts
@@ -119,6 +119,38 @@ export default defineConfig(({ mode }) => {
             },
           ],
         },
+        workbox: {
+          runtimeCaching: [
+            {
+              urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'google-fonts-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                }
+              }
+            },
+            {
+              urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'gstatic-fonts-cache',
+                expiration: {
+                  maxEntries: 10,
+                  maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                },
+              }
+            }
+          ]
+        }
       }),
 
       // https://github.com/intlify/vite-plugin-vue-i18n
